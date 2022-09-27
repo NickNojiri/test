64574c31c55b3ab34343986c889563a341fbfc7f
Nicholas Nojiri <nickthe20@gmail.com>
2022-09-27 00:16:37 -0700

    add DArray member function to DARRAY_H_

--- workspace/cs250/Lab3_DArray/DArray.h
+++ workspace/cs250/Lab3_DArray/DArray.h
@@ -4,6 +4,9 @@ class DArray
 {
 public:
     // member functions    
+    void rotateRange(int x , int y, int z);
+    void rotate(int index);
+    
 private:
     int *a; // pointer to a dynamic array of type int          
     int capacity;      
